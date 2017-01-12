30dabf12b5ebc6216d2da0013775c31d0fbfed1c
Mateusz Nowak <nmateusz96@gmail.com>
2017-01-12 14:23:45 +0100

    Concurrent package - lock and condition

---
+++ src/lecture9lowlevelproducerconsumer/App.java
@@ -0,0 +1,34 @@
+package lecture9lowlevelproducerconsumer;
+
+/**
+ * Created by Mateusz on 12.01.2017.
+ */
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        Processor processor = new Processor();
+
+        Thread t1 = new Thread(() -> {
+            try {
+                processor.produce();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            try {
+                processor.consume();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+    }
+}
